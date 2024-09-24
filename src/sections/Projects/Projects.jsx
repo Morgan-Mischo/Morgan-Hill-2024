34786cc9c04d030935488e9259a8c8cb0d58216a
Morgan Mischo <goramorgan@gmail.com>
2024-09-24 14:51:59 -0600

    Adding in Github Link

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./ProjectsStyles.module.css";
 import apple_clone from "../../assets/apple_clone.png";
+import apple_clone from "../../assets/apple_clone.png";
 
 function Projects() {
   return (
@@ -9,9 +10,18 @@ function Projects() {
       <div className={styles.projectsContainer}>
         <a href="https://flourishing-madeleine-346f0b.netlify.app/">
           <img className="hover" src={apple_clone} alt="Site Picture" />
-          <h3>Apple Clone</h3>
-          <p>Clone of the Apple website using React, Tailwind, and GSap</p>
         </a>
+        <h3>Apple Clone</h3>
+        <p>
+          Clone of the Apple website using React, Tailwind, and GSap
+          <a
+            className="hover"
+            style="text-decoration: underline"
+            href="https://github.com/Morgan-Mischo/apple_site_clone"
+          >
+            Github Link
+          </a>
+        </p>
       </div>
     </section>
   );
